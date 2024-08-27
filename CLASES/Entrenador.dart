cfddc29b7f1a6527f671342afa7abc942e03d1ce
Bran666 <brandonstif229@gmail.com>
2024-08-27 18:40:02 -0500

    w

---
+++ CLASES/Entrenador.dart
@@ -0,0 +1,9 @@
+class Entrenador {
+  String nombre;
+  String email;
+  DateTime fechaNacimiento;
+  int exp = 500;
+  int batallasGandas = 0;
+
+  Entrenador(this.nombre, this.email, this.fechaNacimiento, this.exp, this.batallasGandas);
+}
