c397a411e9c608c777e5e8133c74c1088d76cbae
Bran666 <brandonstif229@gmail.com>
2024-08-31 16:03:03 -0500

    cambios4

--- index.dart
+++ index.dart
@@ -6,13 +6,12 @@ void main() {
   ManejarSenamones manejador = ManejarSenamones();
   manejador.asignarAleatorios();
 
-  Entrenador ash = Entrenador('Ash Ketchum', 'ash@example.com', DateTime(1990, 4, 1), 500, 10);
-  Entrenador misty = Entrenador('Misty', 'misty@example.com', DateTime(1990, 5, 1), 400, 8);
+  Entrenador bran = Entrenador('Brandon', 'bran@gmail.com', DateTime(1990, 4, 1), 500, 10);
+  Entrenador misty = Entrenador('Misty', 'misty@gmail.com', DateTime(1990, 5, 1), 400, 8);
 
-  ash.agregarSenamonPorIndice(manejador, 0); 
+  bran.agregarSenamonPorIndice(manejador, 0); 
   misty.agregarSenamonPorIndice(manejador, 1);
 
-  // Iniciar una batalla
-  Batalla batalla = Batalla(ash, misty, 0, 0);
+  Batalla batalla = Batalla(bran, misty, 0, 0);
   batalla.iniciarBatalla();
 }
